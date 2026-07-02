a47e8c6049a2a2d866402d0a6a662ab9db762e71
Avi <32148378+avi9664@users.noreply.github.com>
2026-07-01 23:48:42 -0700

    adjust alembic version

--- alembic/versions/0022_normalize_infrastructure_address_columns.py
+++ alembic/versions/0022_normalize_infrastructure_address_columns.py
@@ -1,7 +1,7 @@
 """normalize infrastructure address columns
 
-Revision ID: 0018
-Revises: 0017
+Revision ID: 0022
+Revises: 0021
 Create Date: 2026-05-20 23:52:51.451001
 
 """
@@ -12,8 +12,8 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = '0018'
-down_revision: Union[str, Sequence[str], None] = '0017'
+revision: str = '0022'
+down_revision: Union[str, Sequence[str], None] = '0021'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
